b14e3dfaf013817ab26eecbf27c4f219f3b8fb64
Deep871xd <102525444+Deep871xd@users.noreply.github.com>
2023-07-03 17:00:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let h1 = document.createElement('h1');
+h1.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+
+let div = document.getElementById('sizeInfo');
+div.appendChild(h1);
+
+window.onresize = function() {
+  h1.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+};
